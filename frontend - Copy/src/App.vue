75421a483243bd390579ed9ebc32c885a8992c35
ai-product-manager <alvaro_maravi@hotmail.com>
2025-03-21 20:06:02 -0500

    adding and deleting files

--- frontend - Copy/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <main class="min-h-screen py-8">
-      <PromptGuardian />
-    </main>
-  </template>
-  
-  <script setup lang="ts">
-  import PromptGuardian from './components/PromptGuardian.vue';
-  </script>
